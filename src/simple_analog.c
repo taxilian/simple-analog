4410b2be915bd2810055dad401188137f213c647
Richard Bateman <taxilian@gmail.com>
2015-08-30 21:33:30 -0600

    Abstract the colors into constants, change drawing order to put second hand on top

--- src/simple_analog.c
+++ src/simple_analog.c
@@ -1,4 +1,5 @@
 #include "simple_analog.h"
+#include "gcolor_definitions.h"
 
 #include "pebble.h"
 
@@ -10,10 +11,17 @@ static GPath *s_tick_paths[NUM_CLOCK_TICKS];
 static GPath *s_minute_arrow, *s_hour_arrow;
 static char s_num_buffer[4], s_day_buffer[6];
 
+#define k_frontColor GColorBlack
+#define k_secondHandColor GColorRed
+#define k_minuteHandColor GColorLimerick
+#define k_hourHandColor GColorChromeYellow
+#define k_backColor  GColorWhite
+#define k_centerDotColor  GColorRed
+
 static void bg_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, k_backColor);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
-  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_context_set_fill_color(ctx, k_frontColor);
   for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
     gpath_draw_filled(ctx, s_tick_paths[i]);
   }
@@ -32,24 +40,28 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
     .y = (int16_t)(-cos_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.y,
   };
 
-  // second hand
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_draw_line(ctx, second_hand, center);
-
-  // minute/hour hand
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-
-  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
-  gpath_draw_filled(ctx, s_minute_arrow);
-  gpath_draw_outline(ctx, s_minute_arrow);
+  // hour hand
+  graphics_context_set_fill_color(ctx, k_hourHandColor);
+  graphics_context_set_stroke_color(ctx, k_backColor);
 
   gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
   gpath_draw_filled(ctx, s_hour_arrow);
   gpath_draw_outline(ctx, s_hour_arrow);
 
+  // minute hand
+  graphics_context_set_fill_color(ctx, k_minuteHandColor);
+  graphics_context_set_stroke_color(ctx, k_backColor);
+
+  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
+  gpath_draw_filled(ctx, s_minute_arrow);
+  gpath_draw_outline(ctx, s_minute_arrow);
+
+  // second hand
+  graphics_context_set_stroke_color(ctx, k_secondHandColor);
+  graphics_draw_line(ctx, second_hand, center);
+
   // dot in the middle
-  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, k_centerDotColor);
   graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
 }
 
@@ -82,16 +94,16 @@ static void window_load(Window *window) {
 
   s_day_label = text_layer_create(GRect(46, 114, 27, 20));
   text_layer_set_text(s_day_label, s_day_buffer);
-  text_layer_set_background_color(s_day_label, GColorBlack);
-  text_layer_set_text_color(s_day_label, GColorWhite);
+  text_layer_set_background_color(s_day_label, k_backColor);
+  text_layer_set_text_color(s_day_label, k_frontColor);
   text_layer_set_font(s_day_label, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 
   layer_add_child(s_date_layer, text_layer_get_layer(s_day_label));
 
   s_num_label = text_layer_create(GRect(73, 114, 18, 20));
   text_layer_set_text(s_num_label, s_num_buffer);
-  text_layer_set_background_color(s_num_label, GColorBlack);
-  text_layer_set_text_color(s_num_label, GColorWhite);
+  text_layer_set_background_color(s_num_label, k_backColor);
+  text_layer_set_text_color(s_num_label, k_frontColor);
   text_layer_set_font(s_num_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 
   layer_add_child(s_date_layer, text_layer_get_layer(s_num_label));
